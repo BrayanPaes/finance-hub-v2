5882f75a232fdaa8a7c97fa99d45a3e279fe7529
BrayanPaes <brayan29moraes@gmail.com>
2026-04-27 19:18:37 -0300

    feat: implement user login with JWT authentication

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 const pool = require('../config/db');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 exports.register = async (req, res) => {
@@ -19,3 +20,31 @@ exports.register = async (req, res) => {
         res.status(500).json({ error: 'Internal server error.' });
     }
 };
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const [users] = await pool.query('SELECT * FROM users WHERE email = ?', [email]);
+        if (users.length === 0) {
+            return res.status(401).json({ error: 'Invalid email or password.' });
+        }
+
+        const user = users[0];
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ error: 'Invalid email or password.' });
+        }
+
+        const token = jwt.sign(
+            { id: user.id },
+            process.env.JWT_SECRET,
+            { expiresIn: '1d' },
+        );
+
+        res.json({ message: 'Login successful!', token });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error.' });
+    }
+}
